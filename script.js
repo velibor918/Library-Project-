222b4cb174febe5185f7e2f92080d4aba46a4679
velibor918 <velibor918@gmail.com>
2025-10-09 22:24:36 +0200

    Added selectors for form elements and added library arr

---
+++ script.js
@@ -0,0 +1,7 @@
+let formTitle = document.querySelector('#title');
+let formAuthor = document.querySelector('#author');
+let formPageCount = document.querySelector('#pgCount');
+let formRead = document.querySelector('#read');
+let formBtn = document.querySelector('newBookBtn');
+
+let library = [];
