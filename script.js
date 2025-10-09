9987a06c57b427b8acc76ec4667de7652a3f5c94
velibor918 <velibor918@gmail.com>
2025-10-09 22:28:31 +0200

    Added Book constructor

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let formRead = document.querySelector('#read');
 let formBtn = document.querySelector('newBookBtn');
 
 let library = [];
+
+function Book (title, author, pgCount, read) {
+    if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+    this.title = title;
+    this.author = author;
+    this.pgCount = pgCount;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
